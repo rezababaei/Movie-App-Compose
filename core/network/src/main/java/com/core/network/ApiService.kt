a33bd49930b93fdd45f825c7a4c6932a707951f2
Reza Babaei <rzbabaei1@gmail.com>
2023-09-24 11:28:39 +0200

    Update ApiService.kt
    key deleted

--- core/network/src/main/java/com/core/network/ApiService.kt
+++ core/network/src/main/java/com/core/network/ApiService.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
-    //https://api.themoviedb.org/3/search/movie?api_key=f0a750f44b2adc4d7d523365741cc119&query=Harry
+    //https://api.themoviedb.org/3/search/movie?api_key=<>&query=Harry
     @GET("3/search/movie")
     suspend fun getMovieList(
         @Query("api_key") apiKey:String,
